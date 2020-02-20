61bc51de5c68c3406bf299af2785acef489bf688
slzander <staceyzander@gmail.com>
2020-02-20 16:53:40 -0700

    sets up hooks practice with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  console.log(useEffect)
+
+  useEffect(() => {
+    document.title = `Current count: ${count}`
+    let id = setTimeout(() => {
+      setCount(count + 1)
+    }, 1000)
+    return () => clearTimeout(id)
+  }, [count, todos])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Counter App</h1>
+  <h2>{count}</h2>
+     <button onClick={() => setCount(count + 1)}>+</button>
+     <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 }
